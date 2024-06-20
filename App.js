8863e315ba106974fef8d73bc4a8a8933318add6
AkhilaK894032 <144983735+AkhilaK894032@users.noreply.github.com>
2024-06-20 14:21:59 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
-// src/App.js
-import React from 'react';
-import ToDoList from './ToDoList';
-import './App.css';
+import Keypad from "./keypad"
+import './App.css'
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <ToDoList />
-    </div>
-  );
+
+
+      let [input, setInput] = useState("")
+      function handleClick(value) {
+
+            setInput(input + value)
+
+      }
+      function calculate(value) {
+
+            let outputVal = eval(input)
+            setInput(outputVal)
+
+      }
+      function handleClear() {
+
+            setInput("")
+
+      }
+
+      return (
+            <div className="container">
+                  <h1>Calculator App using react</h1>
+                  <div className="caculator">
+                        <input type="text" value={input} className="output" readOnly />
+                        <Keypad handleClick={handleClick} handleClear={handleClear} calculate={calculate}></Keypad>
+
+                  </div>
+
+            </div>
+      )
 }
 
-export default App;
+export default App
